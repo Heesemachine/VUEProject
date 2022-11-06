04d160102efa768782b9c8a934796d9050f25cca
Heesemachine <93719680+Heesemachine@users.noreply.github.com>
2022-11-06 20:19:51 +0200

    VueProject

---
+++ Vue/src/router/index.js
@@ -0,0 +1,50 @@
+import VueRouter from 'vue-router'
+
+import AllFilmsPage from '../pages/AllFilmsPage'
+import FilmPage from '../pages/FilmPage'
+import MainPage from '../pages/MainPage'
+import FilmsLayout from '../pages/FilmsLayout'
+
+export default new VueRouter({
+    mode: 'history',
+    routes: [{
+        path: '/',
+        name: 'main',
+        component: MainPage
+    },
+    {
+        path: '/films',
+        name: 'filmsLayout',
+        component: FilmsLayout,
+        children: [
+          {
+            path: '',
+            name: 'films',
+            component: AllFilmsPage
+          },
+          {
+            path: ':id',
+            name: 'filmPage',
+            component: FilmPage,
+            beforeEnter: (to, from, next) => {
+              if(localStorage.getItem('auth')) {
+                next()
+              } else {
+                next({ name: 'films' })
+              }
+            }
+          },
+          {
+            path: '*/*',
+            redirect: { name: 'films' }
+          },
+        ]
+      },
+      {
+        // path: '*',
+        // name: 'notFound',
+        // component: NotFound
+      },
+        
+    ]
+})
